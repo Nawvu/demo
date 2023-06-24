aa573d89059b1c265f7b73288a71650c8ad190a4
Nawvu <nhnls3005@gmail.com>
2023-06-24 12:00:28 +0700

    update rule

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -35,6 +35,13 @@
 	function deletefoo(food) {
 		deleteDoc(doc(db, 'things', food.id));
 	}
+	function addchat(thing) {
+		if (!(thing == null || thing == '')) {
+			const eventRef = collection(db, 'things');
+			addDoc(eventRef, { name: thing, time: serverTimestamp(), uid: userID }, { merge: true });
+			
+		}
+	}
 </script>
 
 {#if !signIn}
@@ -56,7 +63,9 @@
 		</section>
 	</div>
 {:else if signIn}
-	<div class="flex flex-col items-center min-h-screen max-h-full justify-center bg-white border-solid ">
+	<div
+		class="flex flex-col items-center min-h-screen max-h-full justify-center bg-white border-solid"
+	>
 		<div class="">
 			<section id="whenSignedIn" hidden />
 			<div class="flex flex-col justify-stretch">
@@ -70,15 +79,7 @@
 						class="  rounded-md p-2 drop-shadow-2xl shadow-black border-solid"
 					/>
 					<button
-						on:click={() => {
-							const eventRef = collection(db, 'things');
-							addDoc(
-								eventRef,
-								{ name: thing, time: serverTimestamp(), uid: userID },
-								{ merge: true }
-							);
-							thing = '';
-						}}
+						on:click={addchat(thing)}
 						class="bg-cyan-600 text-white px-3 py-1 font-bold rounded-md hover:scale-105 hover:bg-slate-500 transition-colors duration-300"
 						>Gửi
 					</button>
@@ -86,14 +87,22 @@
 				<div class=" mt-3 flex flex-col border-b items-stretch w-full">
 					{#each foods.reverse() as food}
 						{#if food.data().uid == userID}
-							<div class="flex flex-row justify-between bg-blue-500 rounded-xl p-2 w-full my-1 text-white shadow-cyan-950">
+							<div
+								class="flex flex-row justify-between bg-blue-500 rounded-xl p-2 w-full my-1 text-white shadow-cyan-950"
+							>
 								<p class="font-semibold">{food.data().name}</p>
-								<button on:click={deletefoo(food)} class="font-semibold">delete</button>
+								{#if userID == '1TZEvWyy85ge1b2kOyUT4zZQlOq1'}
+									<button on:click={deletefoo(food)} class="font-semibold">delete</button>
+								{/if}
 							</div>
 						{:else}
-							<div class="flex flex-row justify-between bg-white 0 rounded-xl p-2 w-full my-1 shadow-lg text-blue-900">
+							<div
+								class="flex flex-row justify-between bg-white 0 rounded-xl p-2 w-full my-1 shadow-lg text-blue-900"
+							>
 								<p class="font-semibold">{food.data().name}</p>
-								<button on:click={deletefoo(food)} class="font-semibold">delete</button>
+								{#if userID == '1TZEvWyy85ge1b2kOyUT4zZQlOq1'}
+									<button on:click={deletefoo(food)} class="font-semibold">delete</button>
+								{/if}
 							</div>
 						{/if}
 					{/each}
