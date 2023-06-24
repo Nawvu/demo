6b3e98b4642bc05b04da0fb11a48bd1ef0c05f17
Nawvu <nhnls3005@gmail.com>
2023-06-24 08:51:32 +0700

    add delete

---
+++ src/components/googlelog.svelte
@@ -0,0 +1,20 @@
+<script>
+	import { auth, googleProvider } from '$lib/firebase';
+	import { signInWithPopup } from 'firebase/auth';
+</script>
+
+<div class="flex items-center flex-col">
+	<section
+		id="whenSignedOut"
+		class="
+		 m-10 bg-gray-700 p-5 rounded-xl shadow-gray-600 w-fit "
+	>
+		<button
+			id="signButon"
+			class=" capitalize font-semibold text-white text-4xl"
+			on:click={() => signInWithPopup(auth, googleProvider)}
+		>
+			Sign with google
+		</button>
+	</section>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,43 +1,76 @@
 <script>
 	// @ts-nocheck
-	import { googleProvider, auth, db } from '../lib/firebase';
-	import { signInWithPopup } from 'firebase/auth';
+	import { auth, db } from '$lib/firebase';
 	import { serverTimestamp, collection } from 'firebase/firestore';
-	import { doc, setDoc, query, onSnapshot } from 'firebase/firestore';
+	import { doc, setDoc, query, onSnapshot, deleteDoc } from 'firebase/firestore';
+	import { onDestroy } from 'svelte';
+	import Googlelog from '../components/googlelog.svelte';
 
 	let thing;
 	let unsubscribe;
 	let foods = [];
+	let signIn;
 	const q = query(collection(db, 'things'));
 	unsubscribe = onSnapshot(q, (querySnapshot) => {
-		foods = []
-		querySnapshot.forEach(doc => {
-			foods.push(doc.data() );
-			foods = foods
+		foods = [];
+		querySnapshot.forEach((doc) => {
+			foods.push(doc.data());
+			foods = foods;
 		});
 	});
+	onDestroy(() => {
+		unsubscribe();
+	});
+	auth.onAuthStateChanged((user) => {
+		if (user) {
+			signIn = true;
+		} else {
+			signIn = false;
+		}
+	});
+	function deletefoo(food) {
+		console.log(food.name);
+		deleteDoc(doc(db, 'things', food.name));
+	}
 </script>
 
-<section id="whenSignedOut">
-	<button id="signButon" class="bg-gray-400" on:click={() => signInWithPopup(auth, googleProvider)}>
-		Sign with google
-	</button>
-</section>
-
-<section id="whenSignedIn" hidden />
-<div>
-	<input type="text" name="" id="" bind:value={thing} class=" border rounded-md" />
-	<button
-		on:click={() => {
-			console.log(thing);
-			const eventRef = doc(db, 'things', thing);
-			setDoc(eventRef, { food: thing, time: serverTimestamp() }, { merge: true });
-		}}>Add</button
-	>
-	<div class="text-black">
-		{#each foods as food(food) }
-			<p>{food.food}</p>
-		{/each}
-		<p>Hi</p>
+{#if !signIn}
+	<Googlelog />
+{:else if signIn}
+	<div class="flex flex-col items-center h-screen justify-center">
+		<div class="">
+			<button
+				on:click={() => {
+					auth.signOut();
+				}}
+				class="bg-orange-400 text-white font-bold p-2 rounded-xl m-3"
+				>Sigg Out
+			</button>
+			<section id="whenSignedIn" hidden />
+			<div class="flex flex-col justify-stretch">
+				<div>
+					<input type="text" name="" id="" bind:value={thing} class="  rounded-md" />
+					<button
+						on:click={() => {
+							console.log(thing);
+							const eventRef = doc(db, 'things', thing);
+							setDoc(eventRef, { name: thing, time: serverTimestamp() }, { merge: true });
+							thing = '';
+						}}
+						class="bg-blue-950 text-white px-3 font-bold rounded-md hover:scale-105 hover:bg-slate-500 transition-colors duration-300"
+						>Add
+					</button>
+				</div>
+				<div class="bg-white mt-3 p-3 flex flex-col border-b items-stretch w-full">
+					{#each foods as food (food)}
+						<div class="flex flex-row justify-between">
+							<p class="font-semibold">{food.name}</p>
+							<button on:click={deletefoo(food)}>Delete</button>
+						</div>
+					{/each}
+					<p>Hi</p>
+				</div>
+			</div>
+		</div>
 	</div>
-</div>
+{/if}
